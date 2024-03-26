0617f57798d6c6f1bdaebfbeaee07ca4bae53465
DELL <omnianabil1920@gmail.com>
2024-03-26 09:16:40 +0200

    adding sounds & app finished

--- lib/widgets/tune_item.dart
+++ lib/widgets/tune_item.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:tune_player_app/models/tune_model.dart';
 
 class TuneItem extends StatelessWidget {
-  const TuneItem({super.key, required this.color});
-  final Color color;
+  const TuneItem({super.key, required this.tuneModel});
+  final TuneModel tuneModel;
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Container(
-        color: color,
+      child: InkWell(
+        onTap: (){
+          tuneModel.playSound();
+          },
+        child: Container(
+          color: tuneModel.color,
+        ),
       ),
     );
   }
